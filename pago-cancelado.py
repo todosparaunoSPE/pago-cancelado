b23056da931119f91c7376bac8a051ce4a7cbf6e
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-06-19 10:06:11 -0600

    Add files via upload

---
+++ pago-cancelado.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 10:03:24 2024
+
+@author: jperezr
+"""
+
+import streamlit as st
+
+# Título de la Aplicación
+st.title('Pago Cancelado')
+
+# Mensaje de Pago Cancelado
+st.header("Pago Cancelado")
+st.write("El pago ha sido cancelado. Inténtalo de nuevo.")
